fa8cd5d86b198f9695ed120df57ff836b3c91722
tommy <tooto1985@gmail.com>
2017-09-10 19:52:11 +0800

    modify

---
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Content/site.css",
+                      "~/Content/StyleSheet1.css"));
+        }
+    }
+}
---
+++ WebApplication1/Controllers/CommonController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CommonController : Controller
+    {
+        // GET: Common
+        public ActionResult Index()
+        {
+            List<string> list = new List<string>();
+            list.Add("aaaa");
+            list.Add("bbbb");
+            list.Add("cccc");
+            TempData["cart"] = "gold";
+            return View(list);
+        }
+
+        public ActionResult partial() {
+            List<string> list = new List<string>();
+            list.Add("xxxx");
+            list.Add("yyyy");
+            list.Add("zzzz");
+            return PartialView(list);
+        }
+
+    }
+}
